67e7c9bd8bbfc5d76b2f26b43f517bd9a7ce8e07
Joseph Garcia <josephdgb1996@gmail.com>
2022-08-03 13:32:49 -0500

    changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 // mongodb
 const { MongoClient } = require('mongodb');
-const url = `mongodb://172.31.90.240:27017`;
+const url = `mongodb://44.202.135.119:27017,3.93.82.62:27017,3.87.54.8:27017/?replicaSet=rs0`;
+// const url = `mongodb://172.31.90.240:27017`;
 const client = new MongoClient(url);
 
 
@@ -21,6 +23,8 @@ const emojis = {
   9: "😏",
 }
 
+
+app.use(cors())
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: false }));
@@ -28,24 +32,53 @@ app.use(express.json());
 
 app.set('views', './public');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res)  => {
   const portText = port.toString();
   const portChar = portText.charAt(portText.length - 1);
 
   const emojiPosition = parseInt(portChar);
   const emojiFigure = emojis[emojiPosition];
 
-  res.render('index.pug', { message: `Escuchando en el puerto ${port} ${emojiFigure}` });
+  let status = "Conectando..."
+
+  try {
+    await client.connect();
+    status = "Conectado";
+  } catch (error) {
+    status = "Desconectado";
+  }
+
+  res.render('index.pug', { message: `Escuchando en el puerto ${port} ${emojiFigure}`, status: status });
 })
 
 app.get('/database', async (req, res) => {
   try {
-    await client.connect();
+    client = await (await client.connect());
+    console.log(client.db.name);
     return res.status(200).send("Conectado a la base de datos");
   } catch (error) {
     return res.status(200).send(error);
   }
 })
+
+app.get('/message', (req, res) => {
+  const portText = port.toString();
+  const portChar = portText.charAt(portText.length - 1);
+
+  const emojiPosition = parseInt(portChar);
+  const emojiFigure = emojis[emojiPosition];
+  res.status(200).send({ message: `Escuchando en el puerto ${port} ${emojiFigure}` })
+})
+
+app.get('/status', async (req, res) => {
+  try {
+    await client.connect();
+    return res.status(200).send({ status: true });
+  } catch (error) {
+    return res.status(404).send({ status: false });
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
